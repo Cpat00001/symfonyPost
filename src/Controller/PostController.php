d114eff6d5d6378c752be808768bf8b9dc257c8f
Pat <p171982@gmail.com>
2021-07-28 20:45:57 +0100

    'form'

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -3,6 +3,13 @@
 //src/controller/PostController.php
 namespace App\Controller;
 
+use App\Entity\Post;
+use App\Form\Type\PostType;
+
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,7 +18,7 @@ class PostController extends AbstractController
 {
     
     /**
-     * @Route("/posts")
+     * @Route("/posts", name="post_list")
      */
 
     public function index(): Response
@@ -19,5 +26,26 @@ class PostController extends AbstractController
         $msg = "New SymfonyPost";
 
         return $this->render('posts.html.twig',['msg' => $msg ]);
+    }
+    /**
+     * @Route("/new",name="new_post")
+     * Method({"GET","POST"})
+     */
+    public function new(Request $request):Response
+    { 
+        //create new Post();
+        $post = new Post();
+        $post->setName('Pierwszy Post');
+        $post->setContent('some example content here for First Post');
+        $post->setAuthor('Tomek Pierwszy');
+        $post->setCreated(new \DateTime('tomorrow'));
+
+        $form = $this->createForm(PostType::class, $post);
+
+        // rendering form with createView()
+
+        return $this->render('new.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
---
+++ src/Entity/Post.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PostRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=PostRepository::class)
+ */
+class Post
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=5000)
+     */
+    private $content;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $author;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $created;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): self
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getCreated(): ?\DateTimeInterface
+    {
+        return $this->created;
+    }
+
+    public function setCreated(\DateTimeInterface $created): self
+    {
+        $this->created = $created;
+
+        return $this;
+    }
+}
