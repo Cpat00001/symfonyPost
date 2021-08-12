7b0dad7bb2178d9b1104f3df77daa6d3715077a1
Pat <p171982@gmail.com>
2021-08-12 20:03:58 +0100

    'changes'

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,7 +4,7 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\Type\UserType;
-
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,23 +12,20 @@ use Symfony\Component\HttpFoundation\Response;
 class UserController extends AbstractController
 {
     /*
-    * @Route("/register", name="new_user")
-    * 
-    */
-    public function new(Request $request): Response
+     * @Route("/register", name="user_register")
+     *
+     */
+    public function register(Request $request): Response
     {
         //creates a User object
         $user = new User();
         $user->setEmail('test1@abc.com');
         $user->setPassword('abc123');
-        
+
         $form = $this->createForm(UserType::class, $user);
 
-        return $this->render('user/new.html.twig', [
+        return $this->render('user/register.html.twig', [
             'form' => $form->createView(),
         ]);
     }
 }
-
-
-
